4bee21212c6803ee17127e4f82fecb154faf9454
Paulo Andres <pbustamante@utpl.edu.ec>
2024-12-18 11:51:27 -0500

    cambios

--- Ejemplosc3/src/paquete01/Ejemplo04.java
+++ Ejemplosc3/src/paquete01/Ejemplo04.java
@@ -28,18 +28,23 @@ public class Ejemplo04 {
         };
         
         int suma;
+        int sumaTotal = 0;
         
         for (int fila = 0; fila < arreglo1.length; fila++) { // 0<2 // 1<2
             suma = 0;
             for (int col = 0; col < arreglo1[fila].length; col++) { // arreglo1[0].length:3 
                                                                     // 0<3 // 1<3 // 2<3 // 3<3
                 suma = suma + arreglo1[fila][col];
+                sumaTotal = sumaTotal + arreglo1[fila][col];
                      // 0 + arreglo1[0][0] // 0 + 10 // 10
                      // 10 + arreglo1[0][1] // 10 + 20 // 30
                      // 30 + arreglo1[0][2] // 30 + 40 // 70
             }
             System.out.printf("Suma de valores del arreglo: %d\n", suma);
+            // sumaTotal = sumaTotal + suma;
         }
+        System.out.printf("Suma total de valores del arreglo: %d\n",
+                sumaTotal);
         
         // System.out.printf("Suma de valores del arreglo: %d\n", suma);
     }
